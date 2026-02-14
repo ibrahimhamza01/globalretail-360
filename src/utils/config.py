fe75b55f4f544e22e4b1983a5103b68e82db60e7
ibrahimhamza01 <ibrahim.hamza01@gmail.com>
2026-02-13 22:57:15 -0500

    Update config.py

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,17 +1,9 @@
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
-# -----------------------------
-# Project Base Directory
-# -----------------------------
-BASE_DIR = Path(__file__).resolve().parents[2]
-
-# -----------------------------
-# Data Directories
-# -----------------------------
-DATA_DIR = BASE_DIR / "data"
-RAW_DATA_DIR = DATA_DIR / "raw"
-PROCESSED_DATA_DIR = DATA_DIR / "processed"
+# Load environment variables
+load_dotenv()
 
 # -----------------------------
 # Environment
@@ -19,13 +11,21 @@ PROCESSED_DATA_DIR = DATA_DIR / "processed"
 ENV = os.getenv("ENV", "dev")
 
 # -----------------------------
+# Project Paths
+# -----------------------------
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+DATA_DIR = BASE_DIR / "data"
+RAW_DATA_DIR = DATA_DIR / "raw"
+PROCESSED_DATA_DIR = DATA_DIR / "processed"
+
+# -----------------------------
 # PostgreSQL Configuration
 # -----------------------------
-POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
-POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
-POSTGRES_DB = os.getenv("POSTGRES_DB", "globalretail")
-POSTGRES_USER = os.getenv("POSTGRES_USER", "postgres")
-POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "postgres")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 
 POSTGRES_URI = (
     f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}"
